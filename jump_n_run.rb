3e2090fd87ed02a1e3bc92356ace2a060bfde562
felixprograms1205 <felixprograms1205@gmail.com>
2021-01-17 10:38:17 +0800

    changed

--- jump_n_run.rb
+++ jump_n_run.rb
@@ -3,34 +3,31 @@ require 'ruby2d'
 set width: 1200, height: 500
 
 @player = Rectangle.new(
-  x: 600, y: 250,
-  width: 15, height: 15,
+  x: 600, y: 50,
+  width: 15, height: -15,
   color: 'yellow'
 )
 @floor = Rectangle.new(
   x: 0, y: 300,
-  width: 1200, height: 2,
+  width: 1200, height: 30,
   color: 'red'
 )
 @x_speed = 0
 @y_speed = 0
-@gravity = 10
-
-def collided?
-    if @player.y >= @floor.y - 24
-        true
-    else
-        @player.y = @floor.y - 24
-        false
-    end
-end
-
+@gravity = 0.5
+@double_jumped_alllllowed = 2
+@last_jumped = nil
 update do
     @player.x += @x_speed
-    if @y_speed < @gravity
+    if @y_speed <= 3
         @y_speed += @gravity
     end
-    unless collided?
+
+    if ( @player.y + @y_speed ) >= @floor.y  && ( @player.y + @y_speed ) <= (@floor.y + @floor.height)
+        @y_speed = 0
+        @double_jumped_alllllowed = 2
+        @player.y = @floor.y
+    else
         @player.y += @y_speed
     end
 end
@@ -46,8 +43,13 @@ on :key do |event|
     elsif event.key == 'left' && event.type == :up
         @x_speed = 0
     end
-    if event.key == 'up' && event.type == :held
-        @player.y -= 30
+    if event.key == 'up' && event.type == :down && @double_jumped_alllllowed > 0 && (@last_jumped.nil? || @last_jumped - Time.now.to_f <= 0.5)
+        @y_speed = -15
+        @double_jumped_alllllowed -= 1
+        @last_jumped = Time.now.to_f
+    elsif event.key == 'up' && event.type == :up
+        @y_speed = 0
     end
+
 end
 show
